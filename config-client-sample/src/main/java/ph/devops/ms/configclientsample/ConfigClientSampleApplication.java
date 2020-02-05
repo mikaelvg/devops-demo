3d24c8b963915a95f488a65caa46af9c54497171
mikael <mikaelvg@gmail.com>
2020-02-05 13:39:24 +0800

    Added spring profile based configuration

---
+++ config-client-sample/src/main/java/ph/devops/ms/configclientsample/ConfigClientSampleApplication.java
@@ -0,0 +1,13 @@
+package ph.devops.ms.configclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientSampleApplication.class, args);
+	}
+
+}
---
+++ config-client-sample/src/test/java/ph/devops/ms/configclientsample/ConfigClientSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.devops.ms.configclientsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
